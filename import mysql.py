84b16926a02ad3f67ec6819d271a44cfd297e1c5
ASJ978 <austin.s.johannesen@gmail.com>
2024-04-07 18:16:18 -0500

    Working connection + data insert for new accounts

---
+++ import mysql.py
@@ -0,0 +1,11 @@
+import mysql.connector
+conn = mysql.connector.connect(host= '127.0.0.1', user = 'root', passwd="WaffleMan@978", port=3306, database = "testdb")
+cursor = conn.cursor()
+##cursor.execute("CREATE DATABASE testdb")
+cursor.execute("CREATE TABLE test_table (name VARCHAR(255), age INTEGER(10))")
+
+""" testQuery = "SELECT * FROM test_table"
+cursor.execute(testQuery)
+for item in cursor:
+    print(item) """
+##conn.close()
--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 import mysql.connector
-conn = mysql.connector.connect(host= '127.0.0.1', user = 'root', passwd="WaffleMan@978", port=3306)
+conn = mysql.connector.connect(host= '127.0.0.1', user = 'root', passwd="WaffleMan@978", port=3306, database = "bank_data")
+cursor = conn.cursor()
+
+add_data = "INSERT INTO user_info (userName, pwd) VALUES(%s, %s)"
+##conn.close()
+
+
+
+
 
 userName = ""
 pwd = ""
 ##Welcome message
+def main():
+    print("Hello! Welcome to the bank!\n")
+    acctCreate = input("Are you new to this bank? y/n: ")
+ ###Check if need to make account   
+    if acctCreate == "y":
+        newAccnt()
+    if acctCreate == "n":
+        prevAccnt()
 
-print("Hello! Welcome to the bank!\n")
 
-###Check if need to make account
-acctCreate = input("Are you new to this bank? y/n: ")
+
 
 ###If need to make account, create username and password
-if acctCreate == "y":
+def newAccnt():
     userName = input("Please provide a username: ")
     pwd = input("Please provide a password: ")
+    storeInfo = (userName, pwd)
+    cursor.execute(add_data, storeInfo)
+    conn.commit()
     
 ###If not need to make account used previous stored username and pwd
-if acctCreate == "n":
+def prevAccnt():
     checkName = input("Please enter your username: ")
     checkPwd = input("Please enter your pwd: ")
+    if checkPwd != pwd:
+        print("Incorrect password.\n")
+        main()
+    elif pwd == "":
+        print("No password saved, please make an account")
+    
+
+main()
